e3b1c6b2cadcfbaeaff7213a3d4ce7e16ead05a1
cafrmen <cafrmen@users.noreply.github.com>
2021-01-17 21:12:55 -0600

    Fix the forecast of the current location button

--- src/app.js
+++ src/app.js
@@ -154,6 +154,10 @@ function searchLocation(position) {
   let apiKey = "a899961427dc9d3ac320aeea78474c0d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
+
+  //This is the call for the forecast in the current location button
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 function getCurrentLocation(event) {
